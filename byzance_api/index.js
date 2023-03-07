a6f77659784b85106f6bcf1af425b8e195e4a651
Betina Zhou <zh.betina@gmail.com>
2023-03-07 16:38:41 +0100

    Add jwt to byzance api & finder to authenticate

--- byzance_api/index.js
+++ byzance_api/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const pg = require("pg");
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
 
 const hostname = "localhost";
 const port = 8004;
 const app = express();
+dotenv.config();
 
 const pgConnString = "pg://admin:admin@localhost:5432/hotelb";
 const pgConn = new pg.Client(pgConnString);
@@ -13,6 +16,27 @@ pgConn.connect();
 app.use(cors());
 app.use(express.static("public"));
 app.use(express.json());
+
+/** Token functions */
+function generateAccessToken(email) {
+  return jwt.sign({email: email}, process.env.TOKEN_SECRET, { expiresIn: 1800 });
+}
+
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers['authorization']
+  const token = authHeader && authHeader.split(' ')[1]
+
+  if (token == null) return res.sendStatus(401)
+
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    console.log(err)
+    if (err) return res.sendStatus(403)
+    req.user = user
+    next()
+  })
+};
+
+/** Token functions */
 
 app.listen(port, hostname, function () {
   console.log("Server running on http://" + hostname + ":" + port);
@@ -43,7 +67,7 @@ app.get("/chambre/:id", function (req, res) {
   );
 });
 
-app.get("/chambres", function (req, res) {
+app.get("/api/chambres", authenticateToken, function (req, res) {
   pgConn.query(
     `SELECT * FROM chambre`,
     function (error, results, fields) {
@@ -71,6 +95,29 @@ app.get("/user", function (req, res) {
     }
   );
 });
+
+app.post("/auth", function(req, res) {
+  const email = req.body.email;
+  const pwd = req.body.password;
+
+  pgConn.query(
+    `SELECT * FROM utilisateur WHERE email='${email}'`,
+    function (error, results, fields) {
+      if (error) {
+        res.status(500).send();
+      } else {
+        const dbEmail = results.rows[0].email;
+        const dbPassword = results.rows[0].password;
+        if (dbEmail == email && dbPassword == pwd) {
+          const token = generateAccessToken(email);
+          res.json({"token": token}).status(200).send()
+        } else {
+          res.status(401).send()
+        }
+      }
+    }
+  )
+})
 
 app.post("/user", function (req, res) {
   const email = req.body.email;
--- finder_index/hotel/views.py
+++ finder_index/hotel/views.py
@@ -10,35 +10,46 @@ def hotels(request):
     if request.method == 'GET':
 
         ## TOKEN AUTH ##
-        authData = {
-            "email": "zorg@sio.fr",
-            "password": "zorg"
-        }
-        headers = {'Content-type': 'application/json'}
-        url = "http://localhost:8005/auth"
-        amorAuth = requests.post(url, json=authData, headers=headers)
-        resAmor = json.loads(amorAuth.text)
-        token = "Bearer " + resAmor["token"]
+        # authData = {
+        #     "email": "zorg@sio.fr",
+        #     "password": "zorg"
+        # }
+        # headers = {'Content-type': 'application/json'}
+        # url = "http://localhost:8005/auth"
+        # amorAuth = requests.post(url, json=authData, headers=headers)
+        # resAmor = json.loads(amorAuth.text)
+        # token = "Bearer " + resAmor["token"]
 
+
+        authData2 = {
+            "email": "zorg@sio.fr",
+            "password": "zorgoTest"
+        }
+        headers2 = {'Content-type': 'application/json'}
+        url2 = "http://localhost:8004/auth"
+        byzanceAuth = requests.post(url2, json=authData2, headers=headers2)
+        resByzance= json.loads(byzanceAuth.text)
+        token2 = "Bearer " + resByzance["token"]
         ## TOKEN AUTH ##
 
-        headersRooms = {'Authorization': token}
-        amorRooms = requests.get('http://localhost:8005/api/chambres', headers=headersRooms)
-        resAmor = json.loads(amorRooms.text)
-        for room in resAmor:
-            room["hotel"] = 1
+        # headersRooms = {'Authorization': token}
+        # amorRooms = requests.get('http://localhost:8005/api/chambres', headers=headersRooms)
+        # resAmor = json.loads(amorRooms.text)
+        # for room in resAmor:
+        #     room["hotel"] = 1
 
-        byzanceRooms = requests.get('http://localhost:8004/chambres')
+        headersRooms2 = {'Authorization': token2}
+        byzanceRooms = requests.get('http://localhost:8004/api/chambres', headers=headersRooms2)
         resByzance = json.loads(byzanceRooms.text)
         for room in resByzance:
             room["hotel"] = 2
 
-        caraibesRooms = requests.get('http://localhost:5000/chambres')
-        resCaraibes = json.loads(caraibesRooms.text)
-        for room in resCaraibes:
-            room["hotel"] = 3
+        # caraibesRooms = requests.get('http://localhost:5000/chambres')
+        # resCaraibes = json.loads(caraibesRooms.text)
+        # for room in resCaraibes:
+        #     room["hotel"] = 3
         
-        response = resAmor + resByzance + resCaraibes
+        response = resByzance
         jsonString = json.dumps(response)
         return JsonResponse(response, safe=False)
 
